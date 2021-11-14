d91674ccba6195f00cda4388de8d4e0524f937da
Michal Czaplinski <mmczaplinski@gmail.com>
2021-11-14 00:17:45 -0500

    After 11. Resolving and binding

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -60,6 +60,13 @@ public class Lox {
     // Stop if there was a syntax error.
     if (hadError) return;
 
+    // Resolve
+    Resolver resolver = new Resolver(interpreter);
+    resolver.resolve(statements);
+
+    // Stop if there was a resolution error.
+    if (hadError) return;
+
     interpreter.interpret(statements);
   }
 
