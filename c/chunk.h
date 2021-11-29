f286343efdb8c8a713320364b97013df1f0c0803
Michal Czaplinski <mmczaplinski@gmail.com>
2021-11-28 21:14:01 -0500

    Done with 15. Virtual machine

--- c/chunk.h
+++ c/chunk.h
@@ -8,11 +8,16 @@
 typedef enum {
   OP_RETURN,
   OP_CONSTANT,
+  OP_ADD,
+  OP_SUBTRACT,
+  OP_MULTIPLY,
+  OP_DIVIDE,
+  OP_NEGATE,
 } OpCode;
 
 typedef struct {
   int count;
-  int capacity;
+  int capacity;  // hello
   uint8_t* code;
   int* lines;
   ValueArray constants;
